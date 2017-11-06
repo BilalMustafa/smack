183d2544e2b32809f35be568955da8f2031ae162
Muhammad Bilal Mustafa <thest211@hotmail.com>
2017-11-06 16:29:27 +0500

    Added hamburger menu

---
+++ smack/Controller/ChannelVC.swift
@@ -0,0 +1,24 @@
+//
+//  ChannelVC.swift
+//  smack
+//
+//  Created by Bilal Mustafa on 11/6/17.
+//  Copyright © 2017 Bilal Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.revealViewController().rearViewRevealWidth = self.view.frame.width - 60
+
+        
+    }
+
+    
+
+}
---
+++ smack/Controller/ChatVC.swift
@@ -0,0 +1,34 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by Bilal Mustafa on 11/6/17.
+//  Copyright © 2017 Bilal Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    
+    //outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+
+    
+    }
+
+   
+    
+
+
+}
